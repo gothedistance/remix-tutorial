ae761ad4d2411cc17f7e798bb18872baae0156a7
YUMOTO Michitaka <michitaka@quality-start.in>
2023-10-31 16:58:35 +0900

    remix tutorial sample has done. https://remix.run/docs/en/main/start/tutorial#optimistic-ui

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,60 @@
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Form,
   Links,
   LiveReload,
   Meta,
+  NavLink,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+  useSubmit
 } from "@remix-run/react";
+import { useEffect } from "react";
+import appStylesHref from "./app.css";
+import { createEmptyContact, getContacts } from "./data";
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+// .<Form> prevents the browser from sending the request to the server
+//and sends it to your route's action function instead with fetch.
+export const action = async () => {
+  const contact = await createEmptyContact()
+  return json({ contact })
+}
+
+export const loader = async ({request}: LoaderFunctionArgs) => {
+  const url = new URL(request.url)
+  const q = url.searchParams.get("q")
+  const contacts = await getContacts(q);
+  // creating application/json responses
+  return json({contacts, q})
+}
 
 export default function App() {
+  //useLoaderData フックが返すデータの型が loader 関数の戻り値の型と同じであることを型推論で取得。
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+  const searching =
+  navigation.location &&
+  new URLSearchParams(navigation.location.search).has(
+    "q"
+  );
+
+  // 検索して戻るボタンで戻った時、検索結果は正しいが入力値がキープされる
+  // それを防ぐために、戻るボタンでURLが変更されたら、検索欄の値を同期する
+  useEffect(() => {
+    const searchField = document.getElementById("q")
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || ""
+    }
+
+  }, [q])
   return (
     <html lang="en">
       <head>
@@ -18,34 +65,61 @@ export default function App() {
       </head>
       <body>
         <div id="sidebar">
-          <h1>Remix Contacts</h1>
+          <h1>Remix Contacts</h1>          
           <div>
-            <Form id="search-form" role="search">
+            <Form id="search-form" role="search"
+              onChange={(event) => { 
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch
+                })
+              }}>
               <input
                 id="q"
                 aria-label="Search contacts"
+                className={searching ? "loading" : ""}
                 placeholder="Search"
                 type="search"
+                defaultValue={q || ""}
                 name="q"
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div id="search-spinner" aria-hidden hidden={!searching} />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </NavLink>
+  
+                  </li>
+                ))}
+             </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
-
+        <div id="detail">
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
